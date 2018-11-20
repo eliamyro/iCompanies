90a5e0d074761316eb8cd293b1458bdfd12a7530
eliamyro <elias.myronidis@hotmail.com>
2018-11-20 21:09:48 +0200

    • Batch Delete Request

--- iCompanies/CompaniesController.swift
+++ iCompanies/CompaniesController.swift
@@ -12,6 +12,18 @@ import CoreData
 class CompaniesController: UITableViewController {
     private let cellId = "cellId"
     var companies = [Company]()
+    
+    lazy var resetBarButton: UIBarButtonItem = {
+        let barButtonItem = UIBarButtonItem(title: "Reset", style: .plain, target: self, action: #selector(handleResetButton))
+        
+        return barButtonItem
+    }()
+    
+    lazy var addCompanyBarButton: UIBarButtonItem = {
+        let barButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany))
+        
+        return barButtonItem
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +31,9 @@ class CompaniesController: UITableViewController {
         view.backgroundColor = .white
         
         navigationItem.title = "Companies"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany))
+        
+        navigationItem.leftBarButtonItem = resetBarButton
+        navigationItem.rightBarButtonItem = addCompanyBarButton
         
         tableView.backgroundColor = .darkblue
         tableView.separatorColor = .white
@@ -55,6 +69,29 @@ class CompaniesController: UITableViewController {
         
         self.present(navController, animated: true, completion: nil)
     }
+    
+    @objc private func handleResetButton() {
+        print("Reset button pressed")
+        
+        let context = CoreDataManager.shared.persistentContainer.viewContext
+        
+        let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: Company.fetchRequest())
+        
+        do {
+            try context.execute(batchDeleteRequest)
+        } catch let deleteError {
+            print("Failed to batch delete companies: ", deleteError.localizedDescription)
+        }
+        
+        var indexPathsToRemove = [IndexPath]()
+        for (index, _) in companies.enumerated() {
+            let indexPath = IndexPath(row: index, section: 0)
+            indexPathsToRemove.append(indexPath)
+        }
+        
+        companies.removeAll()
+        tableView.deleteRows(at: indexPathsToRemove, with: .left)
+    }
 }
 
 extension CompaniesController {
@@ -67,6 +104,24 @@ extension CompaniesController {
     
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 50
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let view = UIView()
+        let label = UILabel()
+        label.text = "No companies available"
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return companies.count == 0 ? 150 : 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
